ac022ce39e04ef75988afe2c08cf7184690471b0
Jeremy Anifacc <zhangleisuda@gmail.com>
2017-10-19 16:49:44 +0800

    website all

---
+++ website/urls.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import url
+from website import views
+
+urlpatterns = [
+    url(r'^index/$', views.index, name='index'), 
+    # url(r'^home/$', views.home, name='home'),
+]
